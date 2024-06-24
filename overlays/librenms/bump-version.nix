3af4e0b86f6421e08bf1bfaf18bed5538453b82b
Sam Grimee <sgrimee@gmail.com>
2024-06-24 14:56:18 +0000

    librenms 24.5->24.6

--- overlays/librenms/bump-version.nix
+++ overlays/librenms/bump-version.nix
@@ -1,13 +1,13 @@
 final: prev: {
   librenms = prev.librenms.overrideAttrs (oldAttrs: rec {
     pname = "librenms";
-    version = "24.5.0";
+    version = "24.6.0";
     src = prev.fetchFromGitHub {
       owner = "librenms";
       repo = pname;
       rev = "${version}";
-      sha256 = "sha256-Qr5SiNcBS5sTnV0lv8PYG5eXZxWfEfbuMZ70P+LPqNY=";
+      sha256 = "sha256-FCMN9Z3mz1wr/MxSYKojIAxggxOe5JmftEmT6B+v3ds=";
     };
-    vendorHash = "sha256-mrwewYRfVnkeC5onPvZRNtnt7VGm/JuS871W9kct7to=";
+    vendorHash = "sha256-pRPYJs299I8eGIQH5PNk6yqXf6P6EEOFym91UEEFa4Q=";
   });
 }
